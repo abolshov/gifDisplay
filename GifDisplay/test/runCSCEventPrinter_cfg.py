afbbea2999b8745b1c684e896167a3aca21d355d
[yumeng] <[yumeng.guo@cern.ch]>
2025-06-02 05:45:54 +0200

    sampleSpring24mc_success

--- GifDisplay/test/runCSCEventPrinter_cfg.py
+++ GifDisplay/test/runCSCEventPrinter_cfg.py
@@ -11,7 +11,7 @@ process.MessageLogger = cms.Service("MessageLogger",
         #default   = cms.untracked.PSet(limit = cms.untracked.int32(1000000))
         #replace above with suppressing all information and allow my logs(couts):
         default   = cms.untracked.PSet(limit = cms.untracked.int32(0)),
-        #CSCEventPrinter = cms.untracked.PSet(limit = cms.untracked.int32(1000000)) 
+        CSCEventPrinter = cms.untracked.PSet(limit = cms.untracked.int32(1000000)) 
     ),
     cerr = cms.untracked.PSet(
         threshold = cms.untracked.string('WARNING'),
@@ -20,13 +20,13 @@ process.MessageLogger = cms.Service("MessageLogger",
 )
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(10)  # -1 to run all events
+    input = cms.untracked.int32(2)  # -1 to run all events
 )
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-        #"root://cmsxrootd.fnal.gov//store/mc/Phase2Spring24DIGIRECOMiniAOD/DYToLL_M-10To50_TuneCP5_14TeV-pythia8/GEN-SIM-DIGI-RAW-MINIAOD/PU140_Trk1GeV_140X_mcRun4_realistic_v4_ext1-v1/70000/0f1a4693-63c6-461d-b2be-7c966f3c6e37.root"
-        "file:/eos/user/y/yumeng/CMSSW_14_2_0_pre1/gifDisplay/Phase2Spring24_DYToLL_M-10To50/0f1a4693-63c6-461d-b2be-7c966f3c6e37.root"
+        "root://cmsxrootd.fnal.gov//store/mc/Phase2Spring24DIGIRECOMiniAOD/DYToLL_M-10To50_TuneCP5_14TeV-pythia8/GEN-SIM-DIGI-RAW-MINIAOD/PU140_Trk1GeV_140X_mcRun4_realistic_v4_ext1-v1/70000/0f1a4693-63c6-461d-b2be-7c966f3c6e37.root"
+        #"file:/eos/user/y/yumeng/CMSSW_14_2_0_pre1/gifDisplay/Phase2Spring24_DYToLL_M-10To50/0f1a4693-63c6-461d-b2be-7c966f3c6e37.root"
     )
 )
 
