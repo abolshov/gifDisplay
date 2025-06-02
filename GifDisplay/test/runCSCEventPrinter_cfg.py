8b65f10cf1edf70e4da15e6c114296a038a2edf8
[yumeng] <[yumeng.guo@cern.ch]>
2025-06-02 02:31:39 +0200

    noCLCTCollection

---
+++ GifDisplay/test/runCSCEventPrinter_cfg.py
@@ -0,0 +1,22 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("CSCPrint")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 1
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10)  # set to -1 to run all events
+)
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+        "root://cmsxrootd.fnal.gov//store/mc/Phase2Spring24DIGIRECOMiniAOD/DYToLL_M-10To50_TuneCP5_14TeV-pythia8/GEN-SIM-DIGI-RAW-MINIAOD/PU140_Trk1GeV_140X_mcRun4_realistic_v4_ext1-v1/70000/0f1a4693-63c6-461d-b2be-7c966f3c6e37.root"
+    )
+)
+
+process.CSCEventPrinter = cms.EDAnalyzer("CSCEventPrinter",
+    clctTag = cms.InputTag("muonCSCDigis", "MuonCSCCLCTDigi")
+)
+
+process.p = cms.Path(process.CSCEventPrinter)
